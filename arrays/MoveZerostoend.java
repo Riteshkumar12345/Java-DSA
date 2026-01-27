b1da729281f8a44436ea88dbcc7708392d8eaca9
Riteshkumar12345 <ritesh.kumar3_cs22@gla.ac.in>
2026-01-27 14:05:29 +0530

    today code is done

---
+++ arrays/MoveZerostoend.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class MoveZerostoend {
+
+    public static void moveZeroes(int[] nums) {
+        int slow = 0;
+
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0) {
+                int temp = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast] = temp;
+                slow++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println("Before: " + Arrays.toString(nums));
+
+        moveZeroes(nums);
+
+        System.out.println("After: " + Arrays.toString(nums));
+
+        sc.close();
+    }
+}
